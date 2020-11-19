bfbc72732ffa05ab68d241fd1e9e7b8c3132ce61
Maria Patokina <mpatokina@hotmail.com>
2020-11-18 19:06:54 -0500

    my first commit

---
+++ exercises.js
@@ -0,0 +1,39 @@
+const greeting = "Hello, Maria!"; 
+console.log(greeting); //prints Hello, Maria!
+const hello = "Hello, ";
+const name1 = "Angel";
+const name2 = "Jose";
+const name3 = "Maria";
+console.log(hello+name1+'!'); //prints Hello, Angel!
+console.log(hello+name2+'!'); //prints Hello, Jose!
+console.log(hello+name3+'!'); //prints Hello, Maria!
+let a = 20; //slide 12
+let b = 5;
+c = a + b;
+console.log(c); //prints 25
+let d = 15; //slide 13
+let e = 3;
+f= d /e;
+console.log(f); //prints 5
+let m = 1; //slide 14
+let n = 2;
+let o = 3;
+let p = 4;
+let q = 5;
+let r = 6;
+let s = 7;
+let t = 8;
+let u = 9;
+let v = 10;
+aver1 = (m + n + o + p + q) / 5;
+console.log(aver1); //prints 3
+aver2 = (r + s + t + u + v) / 5;
+console.log(aver2); //prints 8
+aver = (aver1 + aver2) / 2;
+console.log(aver); //prints 5.5
+//Week1_Day4_MorningLab
+average1 = (4 + 10 + 22 - 30 + 55) / 5;
+console.log(average1); //prints 12.2
+average2 = (68 + 7 - 22 + 9 + 100) / 5;
+console.log(average2); //prints 32.4
+
